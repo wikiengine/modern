3f868d575bc00772d653bff4dcbcf9bbcbeec562
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-02-05 12:45:34 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+	<nav>
+		<a href="/">{{ CONFIG.logo_text }}</a>
+		&nbsp;|&nbsp;
+		<a href="/RecentChanges">최근 변경</a>
+		&nbsp;|&nbsp;
+		<a href="/RecentDiscuss">최근 토론</a>
+		<!--alpine사용 예제-->
+		<p x-text="$store.state.session.account.uuid"></p>
+	</nav>
+	<main>
+		<div>
+			<h1 v-if="page.data.document && page.viewName !== 'error'">
+				<a href="#">
+					<span v-if="page.data.document.forceShowNamespace !== false" class="namespace">{{ page.data.document.namespace }}: </span>{{ page.data.document.title }}
+				</a>
+				<small v-if="page.viewName === 'edit_edit_request' && page.viewName === 'edit_request'">(편집요청)</small>
+				<small v-if="page.viewName === 'edit' && page.data.body.section">(r{{ page.data.body.baserev }} 문단편집)</small>
+				<small v-if="page.viewName === 'edit' && page.data.body.baserev === '0'">
+                    (새 문서 생성)
+                </small>
+				<small v-if="page.viewName === 'edit'">
+                    (r{{ page.data.body.baserev }} 편집)
+                </small>
+				<small v-if="page.viewName === 'history'">
+                    (역사)
+                </small>
+				<small v-if="page.viewName === 'backlink'">
+                    (역링크)
+                </small>
+				<small v-if="page.viewName === 'move'">
+                    (이동)
+                </small>
+				<small v-if="page.viewName === 'delete'">
+                    (삭제)
+                </small>
+				<small v-if="page.viewName === 'acl'">
+                    (ACL)
+                </small>
+				<small v-if="page.viewName === 'thread'">
+                    (토론)
+                </small>
+				<small v-if="page.viewName === 'thread_list'">
+                    (토론 목록)
+                </small>
+				<small v-if="page.viewName === 'thread_list_close'">
+                    (닫힌 토론)
+                </small>
+				<small v-if="page.viewName === 'thread_request_list'">
+                    (닫힌 편집 요청)
+                </small>
+				<small v-if="page.viewName === 'diff'">
+                	(비교)
+                </small>
+				<small v-if="page.viewName === 'revert' && page.data.rev">
+                    (r{{  page.data.rev  }}로 되돌리기)
+                </small>
+				<small v-if="page.viewName === 'raw' && page.data.rev">
+                    (r{{  page.data.rev  }} RAW)
+                </small>
+				<small v-if="page.viewName === 'blame' && page.data.rev">
+                    (r{{  page.data.rev  }} Blame)
+                </small>
+				<small v-if="page.viewName === 'wiki' && page.data.rev">
+                    (r{{  page.data.rev  }} 판)
+                </small>
+			</h1>
+			<h1 v-else>{{ page.title }}</h1> 
+		</div>
+		<div v-html="contentHtml" />
+	</main>
+	<footer>
+		<a href="//testwiki.hyonsu.com">the tree</a>
+	</footer>
+</template>
+<style scoped>
+main {
+	margin: 2px;
+}
+.namespace {
+	color: #8d8d8d;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp, reactive } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+
+//전역변수
+app.config.globalProperties.contentHtml = reactive(window.contentHtml);
+app.config.globalProperties.page = reactive(window.page);
+//반응형 필요없음
+app.config.globalProperties.session = window.session;
+app.config.globalProperties.CONFIG = window.CONFIG;
+
+app.mount('#viteprovider')
